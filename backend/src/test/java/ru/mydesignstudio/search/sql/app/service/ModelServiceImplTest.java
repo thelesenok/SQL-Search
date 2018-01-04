5fb0daeaa2235ec3ab4742ab292cb5c1eeb8ca69
Aleksandr Barmin <barmin.alexander@gmail.com>
2018-01-04 15:07:39 +0300

    Added tests for data model, added Bitbucket pipeline

--- backend/src/test/java/ru/mydesignstudio/search/sql/app/service/ModelServiceImplTest.java
+++ backend/src/test/java/ru/mydesignstudio/search/sql/app/service/ModelServiceImplTest.java
@@ -3,17 +3,28 @@ package ru.mydesignstudio.search.sql.app.service;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import ru.mydesignstudio.search.sql.app.AppApplication;
 import ru.mydesignstudio.search.sql.app.model.TypeDefinition;
 import ru.mydesignstudio.search.sql.app.service.model.ModelService;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.NONE,
+        classes = AppApplication.class
+)
 public class ModelServiceImplTest {
     @Autowired
     private ModelService modelService;
@@ -25,4 +36,37 @@ public class ModelServiceImplTest {
         assertThat(types).isNotEmpty();
     }
 
+    @Test
+    public void allModelsWithPrimaryKeys() throws Exception {
+        final Collection<TypeDefinition> allTypes = modelService.findAllTypes();
+        final Collection<String> errorTypes = new ArrayList<>();
+        for (TypeDefinition type : allTypes) {
+            try {
+                modelService.findPrimaryKeyProperty(type);
+            } catch (Exception e) {
+                errorTypes.add(type.getTypeName());
+            }
+        }
+        assertTrue(String.format(
+                "The following types are invalid: %s",
+                errorTypes
+        ), errorTypes.isEmpty());
+    }
+
+    @Test
+    public void allModelsWithDisplayAttributes() throws Exception {
+        final Collection<TypeDefinition> allTypes = modelService.findAllTypes();
+        final Collection<String> errorTypes = new ArrayList<>();
+        for (TypeDefinition type : allTypes) {
+            try {
+                modelService.findDisplayProperty(type);
+            } catch (Exception e) {
+                errorTypes.add(type.getTypeName());
+            }
+        }
+        assertTrue(String.format(
+                "The following types are invalid: %s",
+                errorTypes
+        ), errorTypes.isEmpty());
+    }
 }
